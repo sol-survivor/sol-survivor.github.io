111b7df45f3f901982cf5ecac6c185fe9190e20b
sol-survivor <sol-survivor@sol-talk.com>
2021-08-23 02:36:33 -0400

    Update to warp speed

--- _next/static/OlyDkp5IvnNfHV1E4GI-J/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-9dac1d136998b25818f8.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
